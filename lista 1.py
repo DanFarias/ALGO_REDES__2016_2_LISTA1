0a6d072c6486b721d3f68d2f7cd4c01dd1e76a25
DanFarias <daniel-henri-@hotmail.com>
2016-08-18 23:43:02 -0300

    Primeira Lista
    Primeira Lista de programação Python

---
+++ lista 1.py
@@ -0,0 +1,32 @@
+#Questão 01
+print('Hello Word')
+
+#Questão 02
+a = "Eu tenho um sonho. O sonho de ver meus filhos julgados por sua personalidade, não pela cor de sua pele."
+print(a.upper())
+print(a.lower())
+print(a.title())
+
+#Questão 03
+a = 200
+b = 250
+c = a + b
+print("O resultado é %d " % c)
+
+a = 200
+b = 8
+c = a / b
+print("O resultado é %d " % c)
+
+a = 200
+b = 7
+c = a * b
+print("O resultado é %d " % c)
+
+a = 800
+b = 10
+c = a - b
+print("O resultado é %d " % c)
+
+a = 2**10
+print("O resultado é %d " % a)
